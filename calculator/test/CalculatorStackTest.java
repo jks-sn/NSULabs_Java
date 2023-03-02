f713fd6c576aaecd5df6dcbb4dcf7a0a8504bfd3
kberkaev <maxotr@mail.ru>
2023-03-02 17:16:22 +0700

    Update CalculatorStackTest.java

--- calculator/test/CalculatorStackTest.java
+++ calculator/test/CalculatorStackTest.java
@@ -1,17 +1,36 @@
 package calculator.test;
 
+import calculator.exceptions.OperatorException;
+import calculator.exceptions.StackException;
 import calculator.logic.CalculatorStack;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class CalculatorStackTest {
-@Test
-    public long getStackLength() {
-    CalculatorStack context = new CalculatorStack();
-    context.push(9);
-    context.clear();
-    Assert.assertEquals(context.getStackLength(),0);
+    @Test
+    public void getStackLength() throws OperatorException {
+        CalculatorStack context = new CalculatorStack();
+        context.push(9);
+        context.clear();
+        Assert.assertEquals(context.getStackLength(), 0);
+        context.clear();
 
-    return 0;
-}
+        context.push(33);
+        context.push(33);
+        Assert.assertEquals(context.getStackLength(), 2);
+        context.clear();
+        try{
+            context.push("abacaba");
+            Assert
+        }
+        catch(StackException ignored){
+
+        }
+
+        context.push(33);
+        context.pop();
+        Assert.assertEquals(context.getStackLength(),0);
+
+
+    }
 }
