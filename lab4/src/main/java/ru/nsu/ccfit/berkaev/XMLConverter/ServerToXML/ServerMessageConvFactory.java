1e27ccc466be812a2e926cbe92c529ac1f80edf7
kberkaev <maxotr@mail.ru>
2023-06-14 15:26:22 +0700

    fix switch

--- lab4/src/main/java/ru/nsu/ccfit/berkaev/XMLConverter/ServerToXML/ServerMessageConvFactory.java
+++ lab4/src/main/java/ru/nsu/ccfit/berkaev/XMLConverter/ServerToXML/ServerMessageConvFactory.java
@@ -1,13 +1,19 @@
 package ru.nsu.ccfit.berkaev.XMLConverter.ServerToXML;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Properties;
 
 import ru.nsu.ccfit.berkaev.XMLConverter.Converter;
 import ru.nsu.ccfit.berkaev.XMLConverter.ConverterFactory;
 import ru.nsu.ccfit.berkaev.exceptions.ConvertionException;
 
+import static ru.nsu.ccfit.berkaev.constants.ErrorConstants.LOADING_DATA_EXCEPTION_MESSAGE;
 import static ru.nsu.ccfit.berkaev.constants.ErrorConstants.NAME_ATTRIBUTE_NAME;
-import static ru.nsu.ccfit.berkaev.constants.SharedConstants.*;
+import static ru.nsu.ccfit.berkaev.constants.XMLConstants.XMLCONVERTERS_PROPETIES_PATH;
 
 public class ServerMessageConvFactory extends ConverterFactory {
     
@@ -20,23 +26,25 @@ public class ServerMessageConvFactory extends ConverterFactory {
     protected Converter createConverter(String serializedXML) throws ConvertionException {
         return parseConverterName(Converter.deserializeDocument(serializedXML).getDocumentElement().getAttribute(NAME_ATTRIBUTE_NAME));
     }
-    protected Converter parseConverterName(String converterName){
-        switch (converterName) {
-            case LOGIN_STATUS_MESSAGE -> {
-                return new LoginStatusConverter();
+    protected Converter parseConverterName(String converterName) {
+        Properties prop = new Properties();
+        try {
+            InputStream stream = ServerMessageConvFactory.class.getResourceAsStream(XMLCONVERTERS_PROPETIES_PATH);
+            prop.load(stream);
+            String className = prop.getProperty(converterName);
+            if (className == null)
+            {
+                throw new ConvertionException(LOADING_DATA_EXCEPTION_MESSAGE);
             }
-            case CHAT_HISTORY_MESSAGE -> {
-                return new ChatHistoryConverter();
-            }
-            case ERROR_COMMAND_NAME -> {
-                return new ErrorMessageConverter();
-            }
-            case GET_PARTICIPANT_LIST_COMMAND_NAME -> {
-                return new ParticipantsListConverter();
-            }
-            default -> {
-                return null;
-            }
+            Constructor<Converter> constructor;
+            Class<Converter> commandClass = (Class<Converter>) Class.forName(className);
+            constructor = commandClass.getConstructor();
+            return constructor.newInstance();
+        }
+        catch(IOException | ConvertionException | ClassNotFoundException | InvocationTargetException |
+              NoSuchMethodException | IllegalAccessException | InstantiationException e)
+        {
+            throw new RuntimeException(e);
         }
     }
 }
--- lab4/src/main/java/ru/nsu/ccfit/berkaev/constants/XMLConstants.java
+++ lab4/src/main/java/ru/nsu/ccfit/berkaev/constants/XMLConstants.java
@@ -2,4 +2,5 @@ package ru.nsu.ccfit.berkaev.constants;
 
 public class XMLConstants {
     public static final String XML_YES = "yes";
+    public static final String XMLCONVERTERS_PROPETIES_PATH = "/serverToXMLConverters.properties";
 }
